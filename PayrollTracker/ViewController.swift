180c6ef021f4bb1160bfe93718b03f31ef690134
Emil Lind <emilsen@Emils-iMac.P-2601HN-F1>
2016-10-24 10:54:46 +0200

    Initial Commit

---
+++ PayrollTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PayrollTracker
+//
+//  Created by Emil Møller Lind on 24/10/2016.
+//  Copyright © 2016 Emil Møller Lind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
